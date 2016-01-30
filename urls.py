6787e387464c17bbcedfd124e35d4eeb34177cba
egenerat <emile.generat@outlook.com>
2016-01-30 12:10:44 +0000

    first commit: project structure ongoing

---
+++ urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.defaults import *
+#from django.contrib.auth.forms import AuthenticationForm
+
+#handler500 = 'djangotoolbox.errorviews.server_error'
+
+urlpatterns = patterns('',
+   # (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/guestbook/', }),
+    (r'^fm/', include('fm.urls')),
+
+    #(r'^accounts/login/$', 'django.contrib.auth.views.login',
+    #    {'authentication_form': AuthenticationForm,
+    #    'template_name': 'guestbook/login.html',}),
+    #(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+)
