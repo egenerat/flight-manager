0c4d3af35dd2905d7a1673decbcb214d8a9d76a9
egenerat <emile.generat@outlook.com>
2016-06-07 23:36:35 +0100

    planes bean updates

--- app/planes/RootPlane.py
+++ app/planes/RootPlane.py
@@ -2,3 +2,6 @@ class RootPlane(object):
 
     def __init__(self, plane_id):
         self.__plane_id = plane_id
+
+    def get_id(self):
+        return self.__plane_id
--- app/planes/SupersonicPlane.py
+++ app/planes/SupersonicPlane.py
@@ -5,6 +5,7 @@ from app.planes.PlaneBean import PlaneBean
 class SupersonicPlane(PlaneBean):
 
     limit_change_engines = 96
+    engines_nb = 4
 
     def __init__(self, id, required_maintenance, status, kerozene=None, current_engine_hours=None, maximum_engine_hours=None, km=None):
         super(SupersonicPlane, self).__init__(id, required_maintenance, status, kerozene, current_engine_hours, maximum_engine_hours, km)
