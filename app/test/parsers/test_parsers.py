ca14e5ad2138d12b90fa61257283ea315de9432f
Emile Generat <emile.generat@outlook.com>
2016-06-07 21:40:33 +0100

    tests parsers complete

--- app/test/parsers/test_parsers.py
+++ app/test/parsers/test_parsers.py
@@ -4,6 +4,8 @@ from app.common.target_urls import YOUR_PLANES_URL
 from app.parsers.planesparser import is_regular_plane, build_planes_from_html
 import unittest
 
+from app.planes.planes_util2 import split_planes_list_by_type
+
 
 class TestPlaneType(unittest.TestCase):
 
@@ -21,9 +23,13 @@ class TestParser(unittest.TestCase):
         cls.html_page = requests.get(YOUR_PLANES_URL).text
 
     def test_parser(self):
-        planes_list = build_planes_from_html(self.html_page)
-        print(len(planes_list))
-        self.assertEqual(len(planes_list), 186)
+        self.planes_list = build_planes_from_html(self.html_page)
+        self.assertEqual(len(self.planes_list), 186)
+        planes_by_type = split_planes_list_by_type(self.planes_list)
+        self.assertEqual(len(planes_by_type['supersonic_planes']), 97)
+        self.assertEqual(len(planes_by_type['commercial_planes']), 69)
+        self.assertEqual(len(planes_by_type['root_planes']), 20)
+
 
 if __name__ == '__main__':
     unittest.main()
