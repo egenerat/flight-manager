36eb8b4035946347f9058bea86e6747d79ce9a35
egenerat <emile.generat@outlook.com>
2016-06-07 21:37:19 +0100

    complete planes hierarchy

--- app/planes/CommercialPlane.py
+++ app/planes/CommercialPlane.py
@@ -1,4 +1,9 @@
-from app.planes import PlaneBean
+from app.planes.PlaneBean import PlaneBean
+
 
 class CommercialPlane(PlaneBean):
-    pass
+
+    limit_change_engine = 40
+
+    def __init__(self, plane_id, required_maintenance, status, kerozene=None, current_engine_hours=None, maximum_engine_hours=None, km=None):
+        super(CommercialPlane, self).__init__(plane_id, required_maintenance, status, kerozene, current_engine_hours, maximum_engine_hours, km)
--- app/planes/SupersonicPlane.py
+++ app/planes/SupersonicPlane.py
@@ -1,10 +1,16 @@
+from app.common.constants import CHANGE_HOUR_SUPERSONIC
+from app.planes.PlaneBean import PlaneBean
 
-class CommercialPlane(PlaneBean):
+
+class SupersonicPlane(PlaneBean):
+
+    limit_change_engines = 96
+
+    def __init__(self, id, required_maintenance, status, kerozene=None, current_engine_hours=None, maximum_engine_hours=None, km=None):
+        super(SupersonicPlane, self).__init__(id, required_maintenance, status, kerozene, current_engine_hours, maximum_engine_hours, km)
+
+    def is_required_maintenance(self):
+        pass
 
     def engines_to_be_changed(self):
         return self.__current_engine_hours >= CHANGE_HOUR_SUPERSONIC
-
-    def get_value(self):
-        if self.__km and self.__kerozene:
-            return get_concorde_value(self.__km, self.__kerozene)
-        return None
