e5929515c7fa69b17f6eda8a42ace7f53ffe3d7e
Emile Generat <emile.generat@outlook.com>
2016-06-07 23:36:35 +0100

    planes bean updates

--- app/planes/CommercialPlane.py
+++ app/planes/CommercialPlane.py
@@ -4,6 +4,7 @@ from app.planes.PlaneBean import PlaneBean
 class CommercialPlane(PlaneBean):
 
     limit_change_engine = 40
+    engines_nb = 2
 
     def __init__(self, plane_id, required_maintenance, status, kerozene=None, current_engine_hours=None, maximum_engine_hours=None, km=None):
         super(CommercialPlane, self).__init__(plane_id, required_maintenance, status, kerozene, current_engine_hours, maximum_engine_hours, km)
