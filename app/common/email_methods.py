fd9e1ee79fc0a7d6e9a2c3ed771758d2c1b1e5ef
Emile Generat <emile.generat@outlook.com>
2016-06-07 21:38:51 +0100

    updates import

--- app/common/email_methods.py
+++ app/common/email_methods.py
@@ -1,6 +1,6 @@
-from app.common import constants
+from app.common.constants import NOTIFIED_EMAIL_ADDRESS, EMAIL_ADDRESS
 from django.core.mail import send_mail
 
 
 def notify(title, body):
-    send_mail(title, body, constants.EMAIL_ADDRESS, [constants.NOTIFY_EMAIL], fail_silently=False)
+    send_mail(title, body, EMAIL_ADDRESS, [NOTIFIED_EMAIL_ADDRESS], fail_silently=False)
