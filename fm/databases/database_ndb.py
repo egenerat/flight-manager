48a41c40bc1ca8141c0c6737f171ca763b396e76
egenerat <emile.generat@outlook.com>
2016-06-12 20:52:40 +0100

    structure fm package

---
+++ fm/databases/database_ndb.py
@@ -0,0 +1,29 @@
+from fm.models import Mission
+
+
+def db_remove_all_missions():
+    #     db_remove_all_missions()
+    obj_list = Mission.query()
+    for i in obj_list:
+        i.key.delete()
+
+
+def db_get_all_missions():
+    #     Mission.objects.all()
+    return Mission.query()
+
+
+def db_insert_object(obj):
+    #     obj.save()
+    obj.put()
+
+
+def db_count_missions():
+    #     Mission.objects.count()
+    len(db_get_all_missions())
+    return 0
+
+
+def db_get_ordered_missions(origin_country, speed, capacity):
+    raise Exception('Mission not implemented')
+    # return Mission.objects.all().filter(travellers_nb__lte=capacity).order_by('-revenue_per_hour')
