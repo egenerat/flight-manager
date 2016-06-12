2cf922a6689017ddd2af1f45df6d255233183171
Emile Generat <emile.generat@outlook.com>
2016-06-12 20:54:27 +0100

    templates and mission list v2

---
+++ fm/list_missions.py
@@ -0,0 +1,17 @@
+from app.common.http_methods import post_request
+from app.common.target_urls import SUPERSONICS_MISSION_PAGE
+from app.missions.missionparser import parse_all_missions_in_page, get_country_list
+
+
+def list_missions():
+    result = []
+    supersonics_country = get_country_list()
+    for country_id in supersonics_country:
+        page = post_request(SUPERSONICS_MISSION_PAGE, {u'id_pays': country_id})
+        temp = parse_all_missions_in_page(page, country_id)
+        result.append(temp)
+    return result
+
+
+
+
