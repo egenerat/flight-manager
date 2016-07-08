4f0d23647561fd2de5f8bd904351291e1f9b6062
egenerat <emile.generat@outlook.com>
2016-07-08 22:38:14 +0100

    plane maintainer

--- app/planes/plane_maintainer.py
+++ app/planes/plane_maintainer.py
@@ -2,7 +2,7 @@ from app.common import logger
 from app.common.email_methods import notify
 from app.common.http_methods import get_request, post_request
 from app.common.string_methods import string_contains, exception_if_not_contains
-from app.common.target_urls import PLANES_PAGE, SITE
+from app.common.target_urls import SITE
 
 
 class PlaneMaintainer(object):
@@ -14,29 +14,23 @@ class PlaneMaintainer(object):
         # TODO make sure airport contains enough resources before maintenance
 
     def __change_engines(self):
-        if self.engines_to_be_changed():
-            get_request(PLANES_PAGE)
-            get_request('{}/compte.php?page=action&action=8&id_avion={}'.format(SITE, self.plane.plane_id))
-            result = post_request('{}/compte.php?page=action&action=9&id_avion={}'.format(self.plane.plane_id),
-                                  {'id_moteur': self.plane.replacement_engines_type})
-            if not string_contains("L'avion va bien recevoir ses nouveaux moteurs, dur&eacute;e : 2 heures.", result):
-                logger.warning('Error while changing engines')
-            self.ready = False
+        result = post_request('{}/compte.php?page=action&action=9&id_avion={}'.format(SITE, self.plane.plane_id),
+                              {'id_moteur': self.plane.replacement_engines_type})
+        if not string_contains("L'avion va bien recevoir ses nouveaux moteurs, dur&eacute;e : 2 heures.", result):
+            # TODO parse the answer
+            logger.warning('Error while changing engines')
+        self.__ready = False
 
     def __fill_fuel(self):
-        try:
-            fuel_qty = self.plane.fuel_capacity - self.plane.kerozene
-            confirm_page = post_request('{}/compte.php?page=action&action=7&id_avion={}'.format(SITE, self.plane.plane_id),
-                                        {'cq': fuel_qty})
-            exception_if_not_contains(
-                'Vous avez ajout&eacute; .+ litres? de k&eacute;ros&egrave;ne dans votre avion !', confirm_page)
-        except:
-            logger.error('Problem when filling kerozene')
-            if self.__kerozene < MINIMUM_KEROZENE_BEFORE_MISSION:
-                self.__ready = False
+        fuel_qty = self.plane.fuel_capacity - self.plane.kerozene
+        confirm_page = post_request('{}/compte.php?page=action&action=7&id_avion={}'.format(SITE, self.plane.plane_id),
+                                    {'cq': fuel_qty})
+        if not string_contains('Vous avez ajout&eacute; .+ litres? de k&eacute;ros&egrave;ne dans votre avion !', confirm_page):
+            logger.warning('Error when filling fuel')
+            self.__ready = False
 
     def __scrap_plane(self):
-        get_request(SITE + '/compte.php?page=action&action=10&id_avion=' + self.plane.plane_id)
+        get_request('{}/compte.php?page=action&action=10&id_avion={}'.format(SITE, self.plane.plane_id))
 
     def __do_maintenance(self):
         page = None
@@ -50,6 +44,7 @@ class PlaneMaintainer(object):
             # TODO I think all this condition is met all the time
             if not string_contains("en mission, en maintenance ou n'a pas plus de 100,000 km sans maintenance",
                                    page):
+                # case when the current airport has changed
                 notify('AS : could not send to maintainance', page)
             else:
                 raise OutdatedPlanesListException()
@@ -59,10 +54,10 @@ class PlaneMaintainer(object):
         # if self.is_required_maintenance():
         #     self.__do_maintenance()
         #     return False
-        if self.plane.engines_to_be_changed():
-            self.__change_engines()
-        # if self.is_fuel_full():
-        #     self.__fill_fuel()
+        # if self.plane.engines_to_be_changed():
+        #     self.__change_engines()
+        if not self.plane.is_fuel_full():
+            self.__fill_fuel()
         return self.__ready
 
-# TODO: It could be interesting to have a parent class that handles all the planes, and define how many engines are required, etc
+# TODO: It could be interesting to have a parent class that defines how many engines are required, etc
