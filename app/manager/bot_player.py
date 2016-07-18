f3ab1276817dcc3705bf087917b7683c86515d0f
Emile Generat <emile.generat@outlook.com>
2016-07-18 23:31:02 +0100

    working simplified send missions

--- app/manager/bot_player.py
+++ app/manager/bot_player.py
@@ -14,6 +14,7 @@ class BotPlayer(object):
         self.airport = self.build_airport()
         self.planes = self.build_planes()
         self.ongoing_missions = self.get_ongoing_missions()
+        # TODO move sessions retrieval to the upper class
         self.missions = self.get_missions()
 
     def build_airport(self):
@@ -24,59 +25,27 @@ class BotPlayer(object):
     def build_planes(self):
         html_page = get_request(PLANES_PAGE)
         planes_list = build_planes_from_html(html_page)
-        planes_by_type = split_planes_list_by_type(planes_list)
-        return planes_by_type
+        sorted_planes = split_planes_list_by_type(planes_list)
+        return sorted_planes
 
     def get_ongoing_missions(self):
         dashboard = get_request(MISSION_DASHBOARD)
         return get_ongoing_missions(dashboard)
 
-    def get_mock_missions(self):
-        return {1L: [1978L, 1983L, 1973L, 1991L, 1967L, 1958L, 2008L, 1990L, 2000L, 1959L, 1953L], 7L: [1536L, 1535L],
-                  12L: [1539L, 1538L, 1537L], 205L: [1916L, 1917L, 1915L, 1914L, 1918L],
-                  154L: [2262L, 2276L, 2263L, 2253L, 2264L, 2236L],
-                  155L: [1802L, 1805L, 1804L, 1801L, 1807L, 1803L, 1808L, 1806L], 158L: [1820L, 1822L, 1811L],
-                  164L: [2853L, 2820L, 2836L, 2864L, 2819L, 2865L, 2835L, 2806L, 2805L, 2859L, 2818L],
-                  40L: [1583L, 1587L, 1585L, 1586L, 1588L, 1584L], 41L: [1590L, 1594L, 1596L, 1592L, 1591L],
-                  177L: [1872L], 50L: [1624L, 1625L, 1627L, 1623L, 1626L, 1622L],
-                  51L: [1881L, 1885L, 1882L, 1878L, 1879L, 1883L, 1880L], 185L: [1898L, 1902L, 1906L, 1903L],
-                  62L: [1650L, 1656L, 1652L, 1651L, 1658L, 1654L],
-                  63L: [1662L, 1663L, 1660L, 1665L, 1661L, 1666L, 1664L], 68L: [1692L],
-                  197L: [1910L, 1909L, 1908L, 1913L, 1907L, 1911L, 1912L], 77L: [2500L, 2488L, 2523L],
-                  79L: [1719L, 1722L, 1720L, 1723L, 1721L], 212L: [2154L, 2174L, 2139L, 2145L, 2168L],
-                  36L: [2211L, 2204L, 2205L, 2210L, 2223L, 2228L], 227L: [1934L], 102L: [2324L, 2323L],
-                  105L: [2723L, 2667L, 2722L, 2692L, 2707L, 2700L, 2713L, 2681L, 2673L, 2660L, 2680L, 2679L],
-                  113L: [1767L, 1773L, 1771L, 1774L, 1770L, 1769L, 1768L, 1772L], 114L: [1778L, 1776L, 1775L],
-                  121L: [2593L, 2623L, 2610L, 2655L, 2648L]}
-
     def get_missions(self):
-        return sort_missions_by_type(db_get_ordered_missions_multi_type(200, '-reputation_per_hour'))
-    ## jet_missions = all_missions['5']
+        return db_get_ordered_missions_multi_type(200, '-reputation_per_hour')
+
+    @property
+    def usable_planes(self):
+        return self.planes['commercial_ready_planes'] + self.planes['supersonic_ready_planes'] + self.planes['jet_ready_planes']
 
     def launch_missions(self):
-        mission_list = self.missions['5']
+        mission_list = self.missions
         ongoing_missions = self.ongoing_missions
-
         mission_list = subtract(mission_list, ongoing_missions)
-
-        missions_json = {}
-        for i in mission_list:
-            if i.country_nb not in missions_json:
-                missions_json[i.country_nb] = []
-            missions_json[i.country_nb].append(i.mission_nb)
-
-        # purge empty countries
-        result = {}
-        for i in missions_json:
-            if missions_json[i]:
-                result[i] = missions_json[i]
-        jet_list = self.planes['jet_ready_planes']
-        ready_planes = jet_list
-        PlaneGarage(ready_planes, self.airport).prepare_all_planes()
-        accept_all_missions(result, ready_planes)
-
-
-
+        garage = PlaneGarage(self.usable_planes, self.airport)
+        garage.prepare_all_planes()
+        accept_all_missions(mission_list, garage.ready_planes)
 
 
 
@@ -92,23 +61,6 @@ class BotPlayer(object):
         today = datetime.datetime.now()
         return (expiry_date - today) <= datetime.timedelta(0)
 
-
-def update_missions():
-    switch_to_airport(BANK_AIRPORT)
-    db_remove_all_missions()
-    missions_list = list_missions()
-    current_airport = Airport()
-    country = current_airport.get_country()
-    expiry_date = get_expiry_date()
-    for a_mission_dict in missions_list:
-        a_mission = Mission(**a_mission_dict)
-        a_mission.expiry_date = expiry_date.replace(tzinfo=None)
-        a_mission.revenue_per_hour = get_real_benefit(a_mission)
-        a_mission.origin_country = country
-        total_hours = int(a_mission.time_before_departure + math.ceil(a_mission.km_nb / CONCORDE_SPEED) * 2)
-        a_mission.total_time = total_hours
-        a_mission.reputation_per_hour = int(int(a_mission.reputation) / total_hours)
-        db_insert_object(a_mission)
 
 
 def send_planes():
@@ -198,28 +150,4 @@ def send_planes():
                             current_airport.buy_kerozene(kerozene_litres)
                         except:
                             logger.error("Really can't buy kerozene")
-            # 1 get all mission ordered
-            missions_list = list_missions
-
-            # 2 purge all already ongoing
-            ongoing_missions_id = get_ongoing_missions()
-            missions_list = subtract(missions_list, ongoing_missions_id)
-
-            # 3 cut just the good number
-            missions_list = missions_list[:ready_planes_nb]
-
-            # 4 group missions by country
-            missions_json = {}
-            for i in missions_list:
-                i.country = countries[str(i.country_nb)]
-                if i.country_nb not in missions_json:
-                    missions_json[i.country_nb] = []
-                missions_json[i.country_nb].append(i.mission_nb)
-
-            # purge empty countries
-            result = {}
-            for i in missions_json:
-                if missions_json[i]:
-                    result[i] = missions_json[i]
-            accept_all_missions(result, ready_planes)
     force_save_session_to_db()
--- fm/mission_handler.py
+++ fm/mission_handler.py
@@ -4,6 +4,8 @@ import traceback
 from app.common.logger import logger
 from app.common.http_methods import post_request
 from app.common.target_urls import GENERIC_ACCEPT_MISSION
+from app.missions.mission_utils import split_missions_list_by_type
+from app.planes.planes_util2 import split_planes_list_by_type
 
 
 def extract_bonus_from_page(page):
@@ -11,23 +13,30 @@ def extract_bonus_from_page(page):
 
 
 def __accept_mission(country_id, plane_id, mission_id, mission_type):
+    logger.warning('accept mission : {} with plane {}'.format(mission_id, plane_id))
     page = post_request(GENERIC_ACCEPT_MISSION.format(mission_type = mission_type, mission_id = mission_id, country_id = country_id), {'id_avion': str(plane_id)})
     # Be careful, if re.findall()[0], may introduce list index out of range exception!
     temp_bonus = extract_bonus_from_page(page)
     if len(temp_bonus) == 1:
-        bonus = temp_bonus[0][0] + temp_bonus[0][1]
+        bonus = int(temp_bonus[0][0] + temp_bonus[0][1])
     return bonus
 
+def temp(plane_list, a_mission):
+    plane_id = plane_list.pop().plane_id
+    try:
+        bonus = __accept_mission(a_mission.country_nb, plane_id, a_mission.mission_nb, a_mission.mission_type)
+    except Exception as e:
+        exception_text = traceback.format_exc()
+        logger.error(exception_text)
+        # notify('AS : bug while accepting mission', get_airport().get_airport_name() + '\nThere was a bug while accepting mission :\n' + str(exception_text))
 
-def accept_all_missions(missions_dict, plane_list):
-    for country_id in missions_dict:
-        for mission_id in missions_dict[country_id]:
-            plane_id = plane_list.pop().plane_id
-            mission_type = '5' #missions_dict['']
-            logger.warning('accept mission : {} with plane {}'.format(mission_id, plane_id))
-            try:
-                __accept_mission(country_id, plane_id, mission_id, mission_type)
-            except Exception as e:
-                exception_text = traceback.format_exc()
-                logger.error(exception_text)
-                # notify('AS : bug while accepting mission', get_airport().get_airport_name() + '\nThere was a bug while accepting mission :\n' + str(exception_text))
+
+def accept_all_missions(missions_list, plane_list):
+    sorted_planes = split_planes_list_by_type(plane_list)
+    sorted_missions = split_missions_list_by_type(missions_list)
+    for a_mission in sorted_missions['missions_for_commercial'][:len(sorted_planes['commercial_ready_planes'])]:
+        temp(sorted_planes['commercial_ready_planes'], a_mission)
+    for a_mission in sorted_missions['missions_for_supersonics'][:len(sorted_planes['supersonic_ready_planes'])]:
+        temp(sorted_planes['supersonic_ready_planes'], a_mission)
+    for a_mission in sorted_missions['missions_for_jet'][:len(sorted_planes['jet_ready_planes'])]:
+        temp(sorted_planes['jet_ready_planes'], a_mission)
