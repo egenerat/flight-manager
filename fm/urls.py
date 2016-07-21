1d03fc077ca1b50c3930fa21dfe7dd9cd2191e78
Emile Generat <emile.generat@outlook.com>
2016-07-21 23:50:09 +0100

    updates to the urls

--- fm/urls.py
+++ fm/urls.py
@@ -1,21 +1,26 @@
 from django.conf.urls.defaults import *
+from fm.views import views_finances
+from fm.views import views_test, views_missions
+
 
 urlpatterns = patterns('fm.views',
     #url(r'^admin/', include(admin.site.urls)),
-    (r'^quizz$', 'quizz'),
-    (r'^answer$', 'answer'),
-    (r'^missions$', 'represent_data'),
-    (r'^refresh$', 'refresh'),
-    (r'^start$', 'start'),
-    (r'^stop$', 'stop'),
-    (r'^launch_missions$', 'launch_missions'),
-    (r'^start_launch_missions$', 'start_launch_missions'),
-    (r'^start_launch_missions_no_refresh$', 'start_launch_missions_no_refresh'),
-    (r'^taxes$', 'taxes'),
-    (r'^start_taxes$', 'start_taxes'),
-    (r'^sale_airports$', 'sale_airports'),
-    (r'^test$', 'test'),
-    (r'^fill_kero$', 'fill_kero'),
-    (r'^start_fill_kero$', 'start_fill_kero'),
-    (r'^engines$', 'engines'),
+    # (r'^quizz$', 'quizz'),
+    # (r'^answer$', 'answer'),
+    # (r'^missions$', 'represent_data'),
+    # (r'^refresh$', 'refresh'),
+    (r'^start$', views_missions.parse_missions),
+    # (r'^stop$', 'stop'),
+    (r'^launch_missions$', views_missions.launch_missions),
+    (r'^start_launch_missions$', views_missions.start_launch_missions),
+    (r'^launch_missions$', views_test.view_launch_missions)
+    (r'^taxes$', views_finances.taxes),
+    (r'^start_taxes$', views_finances.start_taxes),
+    # (r'^sale_airports$', 'sale_airports'),
+    # (r'^test$', 'test'),
+
+    # not ready
+    # (r'^fill_kero$', 'fill_kero'),
+    # (r'^start_fill_kero$', 'start_fill_kero'),
+    # (r'^engines$', 'engines'),
 )
