f13a71a8c6ce2cddd77f07b1e6d033a0151b929a
egenerat <emile.generat@outlook.com>
2016-07-23 20:10:23 +0100

    cleanup file

--- app/planes/plane_maintainer.py
+++ app/planes/plane_maintainer.py
@@ -3,7 +3,7 @@ from app.common.logger import logger
 from app.common.email_methods import notify
 from app.common.http_methods import get_request, post_request
 from app.common.string_methods import string_contains, exception_if_not_contains
-from app.common.target_urls import SITE
+from app.common.target_urls import CHANGE_ENGINES_URL, FILL_FUEL_URL, SCRAP_PLANE_URL, MAINTENANCE_URL
 
 
 class PlaneMaintainer(object):
@@ -14,7 +14,7 @@ class PlaneMaintainer(object):
         # TODO make sure airport contains enough resources before maintenance
 
     def __change_engines(self):
-        result = post_request('{}/compte.php?page=action&action=9&id_avion={}'.format(SITE, self.plane.plane_id),
+        result = post_request(CHANGE_ENGINES_URL.format(plane_id=self.plane.plane_id),
                               {'id_moteur': self.plane.replacement_engines_type})
         if not string_contains("L'avion va bien recevoir ses nouveaux moteurs, dur&eacute;e : 2 heures.", result):
             # TODO parse the answer
@@ -23,7 +23,7 @@ class PlaneMaintainer(object):
 
     def __fill_fuel(self):
         fuel_qty = self.plane.fuel_capacity - self.plane.kerosene
-        confirm_page = post_request('{}/compte.php?page=action&action=7&id_avion={}'.format(SITE, self.plane.plane_id),
+        confirm_page = post_request(FILL_FUEL_URL.format(plane_id=self.plane.plane_id),
                                     {'cq': fuel_qty})
         if not string_contains('Vous avez ajout&eacute; .+ litres? de k&eacute;ros&egrave;ne dans votre avion !',
                                confirm_page):
@@ -31,12 +31,12 @@ class PlaneMaintainer(object):
             self.__ready = False
 
     def __scrap_plane(self):
-        get_request('{}/compte.php?page=action&action=10&id_avion={}'.format(SITE, self.plane.plane_id))
+        get_request(SCRAP_PLANE_URL.format(plane_id=self.plane.plane_id))
 
     def __do_maintenance(self):
         page = None
         try:
-            page = get_request('{}/compte.php?page=action&action=30&id_avion={}'.format(SITE, self.plane.plane_id))
+            page = get_request(MAINTENANCE_URL.format(plane_id=self.plane.plane_id))
             exception_if_not_contains('Votre avion est maintenant en maintenance', page)
         except:
             logger.error('Problem sending to maintenance')
@@ -44,13 +44,15 @@ class PlaneMaintainer(object):
                                    page):
                 # case when the current airport has changed
                 # case not enough mecanicians
-                notify('AS : could not send to maintenance', page)
+                notify('FM : could not send to maintenance', page)
             else:
                 # TODO case plane maintenance was over, should continue iteration over planes, refresh and run again
                 raise OutdatedPlanesListException()
         self.__ready = False
 
     def prepare_plane(self):
+        if self.plane.endlife:
+            self.__scrap_plane()
         if self.plane.required_maintenance:
             self.__do_maintenance()
             return False
@@ -60,5 +62,3 @@ class PlaneMaintainer(object):
         if not self.plane.is_fuel_full():
             self.__fill_fuel()
         return self.__ready
-
-        # TODO: It could be interesting to have a parent class that defines how many engines are required, etc
