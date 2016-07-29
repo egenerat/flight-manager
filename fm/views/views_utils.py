4579a4b87bcc235da540adeefdcb44cf213e22c1
Emile Generat <emile.generat@outlook.com>
2016-07-29 22:03:28 +0100

    refactor the async view system

--- fm/views/views_utils.py
+++ fm/views/views_utils.py
@@ -2,7 +2,16 @@
 
 from google.appengine.api import taskqueue
 
+from django.http import HttpResponse
+
 
 def purge_queue():
     q = taskqueue.Queue('default')
     q.purge()
+    return q
+
+def view_generic_async_start(request, view_name):
+    queue = purge_queue()
+    action_url = '/fm/{}'.format(view_name)
+    queue.add(url=action_url)
+    return HttpResponse('Started: {}'.format(action_url))
