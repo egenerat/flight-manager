487fdfd0d47b20b333b3f24f3914006233659ead
Emile Generat <emile.generat@outlook.com>
2016-07-30 20:13:35 +0100

    generic asynchronous view done. generic synchronous view ongoing

--- fm/views/views_finances.py
+++ fm/views/views_finances.py
@@ -10,8 +10,3 @@ def taxes(request):
     fm.singleton_session.session = request.session
     money_before_taxes()
     return HttpResponse('Taxes ok!')
-
-
-def start_taxes(request):
-    taskqueue.add(url='/fm/taxes')
-    return HttpResponse('Started: taxes')
--- fm/views/views_missions.py
+++ fm/views/views_missions.py
@@ -1,43 +1,32 @@
+# coding=utf-8
+
 import traceback
 
 import fm
-from app.common import logger
 from app.common.email_methods import notify
+from app.common.file_methods import force_save_session_to_db, read_saved_session_from_db
+from app.common.logger import logger
 from app.manager.multi_airport_bot import MultiAirportBot
 from django.http import HttpResponse
 from fm.mission_handler import parse_all_missions
-from fm.views.views_utils import purge_queue
-
-# TODO deprecated file
 
 
-def launch_missions(request):
-    fm.singleton_session.session = request.session
+def view_launch_missions(request):
+    fm.singleton_session.session = read_saved_session_from_db()
     try:
         bot = MultiAirportBot()
-        bot.start()()
+        bot.start()
         logger.info('Successful')
     except Exception as e:
         exception_text = traceback.format_exc()
         logger.error(exception_text)
-        notify('AS : There was a bug during execution', 'There was a bug during execution :\n{}'.format(exception_text))
+        notify('FM : There was a bug during execution', 'There was a bug during execution :\n{}'.format(exception_text))
+        raise e
+    finally:
+        force_save_session_to_db()
     return HttpResponse('started')
 
 
-def start_launch_missions(request, taskqueue=None):
-    fm.singleton_session.session = request.session
-    purge_queue()
-    taskqueue.add(url='/fm/launch_missions')
-    return HttpResponse('Started: launch missions')
-
-
-def start_parse_missions(request, taskqueue=None):
-    fm.singleton_session.session = request.session
-    taskqueue.add(url='/fm/refresh')
-    return HttpResponse('Started')
-
-
-def parse_missions(request):
+def view_parse_missions(request):
     parse_all_missions()
     return HttpResponse('Mission parsed')
-
