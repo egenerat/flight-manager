331445952b5a6d5b6b267ca53cd3798ee6de57bf
Emile Generat <emile.generat@outlook.com>
2016-07-30 20:15:39 +0100

    staff parser

---
+++ app/airport/staff_parser.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from app.common.string_methods import get_numeric_values_regex
+from app.common.target_parse_strings import FLIGHT_ATTENDANTS_REGEX, PILOTS_REGEX
+
+
+def get_pilots(page):
+    total_pilots, busy_pilots = get_numeric_values_regex(PILOTS_REGEX, page)
+    return total_pilots, busy_pilots
+
+
+def get_flight_attendants(page):
+    total_flight_attendants, busy_flight_attendants = get_numeric_values_regex(FLIGHT_ATTENDANTS_REGEX, page)
+    return total_flight_attendants, busy_flight_attendants
