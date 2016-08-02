b62f24f47e48e6cff5b19b73c109b93c89ae7102
Emile Generat <emile.generat@outlook.com>
2016-08-02 23:20:52 +0100

    generic views ready, sync and async

--- fm/views/views_maintainance.py
+++ fm/views/views_maintainance.py
@@ -1,14 +1,10 @@
 # coding=utf-8
 
-import fm
-from django.http import HttpResponse
 from fm.fill_kero import fill_all_airports
 
 # TODO cleanup and activate these views
 
 
-def fill_kero(request):
+def fill_kero():
     # only from market
-    fm.singleton_session.session = request.session
     fill_all_airports()
-    return HttpResponse('Done')
