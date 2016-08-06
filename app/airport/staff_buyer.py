f45734130fc958ebcae14d40ea21346978ff8d2c
egenerat <emile.generat@outlook.com>
2016-08-06 15:19:22 +0100

    staff buyer mechanics added

--- app/airport/staff_buyer.py
+++ app/airport/staff_buyer.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from app.common.http_methods import post_request
-from app.common.target_urls import HIRE_FLIGHT_ATTENDANTS_URL, HIRE_PILOTS_URL
+from app.common.target_urls import HIRE_FLIGHT_ATTENDANTS_URL, HIRE_PILOTS_URL, HIRE_MECHANICS_URL
 
 
 def hire_pilots(pilots_nb):
@@ -11,6 +11,7 @@ def hire_pilots(pilots_nb):
 def hire_flight_attendants(flight_attendants_nb):
     post_request(HIRE_FLIGHT_ATTENDANTS_URL, {'cq': flight_attendants_nb})
 
+
 def hire_mechanics(mechanics_nb):
-    #TODO to be implemented
+    post_request(HIRE_MECHANICS_URL, {'cq': mechanics_nb})
     pass
