f08ff1238f9eb58072ec3c03d886f11ccab808d4
Emile Generat <emile.generat@amadeus.com>
2016-08-06 08:58:39 +0100

    Airport checker feature

--- app/airport/airport_checker.py
+++ app/airport/airport_checker.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 
-from app.airport.airport_buyer import buy_missing_planes
-from app.airport.airport_plane_type_config import airport_config_factory
+from app.airport.airport_buyer import buy_missing_planes, buy_kerosene, buy_engines
+from app.airport.airport_config import planes_config_factory, AirportConfig
+from app.airport.staff_buyer import hire_flight_attendants, hire_pilots
+from app.airport.staff_buyer import hire_mechanics
 from app.planes.CommercialPlane import CommercialPlane
 from app.planes.JetPlane import JetPlane
 from app.planes.SupersonicPlane import SupersonicPlane
@@ -18,9 +20,10 @@ class AirportChecker(object):
     def __init__(self, airport, sorted_planes_dict):
         self.airport = airport
         self.sorted_planes_dict = sorted_planes_dict
+        self.airport_config = AirportConfig(self.airport)
 
     def check_missing_planes(self):
-        config = airport_config_factory(self.airport.planes_capacity)
+        config = planes_config_factory(self.airport)
         return {
             CommercialPlane: config.commercials_nb - len(self.sorted_planes_dict['commercial_planes']),
             JetPlane: config.jets_nb - len(self.sorted_planes_dict['jet_planes']),
@@ -31,3 +34,35 @@ class AirportChecker(object):
         for plane_class, missing_units in self.check_missing_planes().iteritems():
             if self.airport.planes_capacity > len(self.sorted_planes_dict.values()) and missing_units > 0:
                 buy_missing_planes(plane_class, missing_units)
+
+    def fix_missing_staff(self):
+        current_staff = self.airport.staff
+        staff_config = self.airport_config.staff_config
+        pilots_diff = staff_config.pilots_nb - current_staff.total_pilots
+        if pilots_diff > 0:
+            hire_pilots(pilots_diff)
+        flight_attendants_diff = staff_config.flight_attendants_nb - current_staff.total_flight_attendants
+        if flight_attendants_diff > 0:
+            hire_flight_attendants(flight_attendants_diff)
+        mechanics_diff = staff_config.mechanics_nb - current_staff.total_mechanics
+        if mechanics_diff > 0:
+            hire_mechanics(mechanics_diff)
+
+    def fix_missing_resources(self):
+        kerosene_config = self.airport_config.resources_config.kerosene
+        if self.airport.kerosene_supply < kerosene_config['min']:
+            qty_needed = kerosene_config['max'] - self.airport.kerosene_supply
+            buy_kerosene(qty_needed)
+        engines_5_nb = self.airport_config.resources_config.engines_5_nb
+        if self.airport.engines_supply['5'] < engines_5_nb['min']:
+            qty_needed = engines_5_nb['max'] - self.airport.engines_supply['5']
+            buy_engines(qty_needed, '5')
+        engines_6_nb = self.airport_config.resources_config.engines_6_nb['6']
+        if self.airport.engines_supply['6'] < engines_6_nb['min']:
+            qty_needed = engines_6_nb['max'] - self.airport.engines_supply
+            buy_engines(qty_needed, '6')
+
+    def fix(self):
+        self.fix_missing_planes()
+        self.fix_missing_staff()
+        self.fix_missing_resources()
