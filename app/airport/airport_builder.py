c09435b808222e3a8ffb11a8e5bd1d334a5e1f15
egenerat <emile.generat@outlook.com>
2016-08-06 12:41:14 +0100

    mechanics built with other staff

--- app/airport/airport_builder.py
+++ app/airport/airport_builder.py
@@ -4,7 +4,7 @@ from app.airport.Airport import Airport
 from app.airport.Staff import Staff
 from app.airport.airports_parsers import get_country, get_money, get_kerosene_supply, get_kerosene_capacity, \
     get_engines_supply, get_planes_capacity, get_airport_name
-from app.airport.staff_parser import get_pilots, get_flight_attendants
+from app.airport.staff_parser import get_pilots, get_flight_attendants, get_mechanics
 
 
 def build_airport(airport_html, staff_html):
@@ -27,7 +27,7 @@ def build_staff(html_page):
     total_flight_attendants, busy_flight_attendants = get_flight_attendants(html_page)
     ready_flight_attendants = total_flight_attendants - busy_flight_attendants
     #TODO fill the correct value
-    total_mechanics = None
+    total_mechanics = get_mechanics(html_page)
     return Staff(total_pilots=total_pilots, total_flight_attendants=total_flight_attendants,
                  total_mechanics=total_mechanics, ready_pilots=ready_pilots,
                  ready_flight_attendants=ready_flight_attendants)
