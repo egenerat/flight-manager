31512971e5cc47f508c40fabddc6820e3fcd370f
Emile Generat <emile.generat@outlook.com>
2016-12-30 23:33:47 +0000

    parent planes bean are now abstract

--- app/test/airport/test_airport_checker.py
+++ app/test/airport/test_airport_checker.py
@@ -2,18 +2,18 @@
 from app.airport.airport_checker import amount_needed
 import unittest
 
-from app.planes.CommercialPlane import CommercialPlane
-from app.planes.JetPlane import JetPlane
-from app.planes.SupersonicPlane import SupersonicPlane
+from app.planes.Commercial7Plane import Commercial7Plane
+from app.planes.JetDSPlane import JetDSPlane
+from app.planes.SupersonicCCPlane import SupersonicCCPlane
 
 
 class TestAirportParser(unittest.TestCase):
 
     def test_amount_needed(self):
         missing_planes = {
-            CommercialPlane: 1,
-            JetPlane: 2,
-            SupersonicPlane: 3,
+            Commercial7Plane: 1,
+            JetDSPlane: 2,
+            SupersonicCCPlane: 3,
         }
         amount = amount_needed(missing_planes)
         self.assertEqual(amount, 43900000)
