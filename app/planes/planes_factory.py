9674d68019703a3f5e37d1695b02194a6bd6a5f5
egenerat <emile.generat@outlook.com>
2016-12-30 22:53:53 +0000

    typo

--- app/planes/planes_factory.py
+++ app/planes/planes_factory.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from app.common.target_parse_strings import JET_F7X
+from app.common.target_parse_strings import JET_F7X, JET_GX, JET_GS, SUPERSONIC_CC, SUPERSONIC_TU
 from app.planes.JetDSPlane import JetDSPlane
 from app.planes.JetGXPlane import JetGXPlane
 from app.planes.SupersonicCCPlane import SupersonicCCPlane
@@ -9,8 +9,8 @@ from app.planes.SupersonicTUPlane import SupersonicTUPlane
 def planes_factory(plane_model):
     return {
         JET_F7X: JetDSPlane,
-        JET GX: JetGXPlane,
-        JET GS: JetGXPlane,
+        JET_GX: JetGXPlane,
+        JET_GS: JetGXPlane,
         SUPERSONIC_CC: SupersonicCCPlane,
         SUPERSONIC_TU: SupersonicTUPlane,
     }.get(plane_model, NotImplementedError)
