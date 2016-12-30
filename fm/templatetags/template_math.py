5c54946bdedb44e7f4ecb7ed0847ad81c46e67df
Emile Generat <emile.generat@outlook.com>
2016-12-30 17:14:25 +0000

    template tag country id to full name

--- fm/templatetags/template_math.py
+++ fm/templatetags/template_math.py
@@ -1,3 +1,4 @@
+from app.common.countries import countries
 from django import template
 
 register = template.Library()
@@ -6,3 +7,11 @@ register = template.Library()
 @register.filter
 def multiply(x, y):
     return float(x) * float(y)
+
+
+@register.filter
+def country_id_to_name(country_id):
+    country_id = str(country_id)
+    return countries[country_id]
+
+
