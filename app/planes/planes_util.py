deb86f93b5a02ceeb10ad0e2316c946584eeba30
Emile Generat <emile.generat@outlook.com>
2016-12-30 19:35:26 +0000

    Util functions to handle plane range properly

--- app/planes/planes_util.py
+++ app/planes/planes_util.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import math
 
 from app.common.constants import MAX_KM, KEROSENE_PRICE
 
@@ -12,6 +13,22 @@ def get_plane_value(new_plane_value, km, kerosene_qty):
     value += kerosene_qty * KEROSENE_PRICE
     return int(value)
 
+
+def duration_mission(distance, speed):
+    return math.ceil(distance/float(speed))
+
+
+def calculate_total_consumption_mission(duration, conso_per_hour, passengers_nb, staff_nb):
+    # formula is flight_hours * (consumption_per_hour + 3*(passengers_nb+staff))*3/2
+    # replacing time by distance/speed
+    return duration*(conso_per_hour+3*(passengers_nb+staff_nb))*(3/2.0)
+
+
+def calculate_real_autonomy_one_way(speed, kerosene_capacity, conso_per_hour, passengers_nb, staff_nb):
+    max_duration = 0
+    while calculate_total_consumption_mission(max_duration, conso_per_hour, passengers_nb, staff_nb) < kerosene_capacity:
+        max_duration+=1
+    return (max_duration - 1) * speed
 
 def is_supersonic(string_model):
     return string_model in SUPERSONICS_MODELS_HTML
