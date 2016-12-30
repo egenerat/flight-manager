dce208dc525b765e57f97001b42ff3fc34dcd384
Emile Generat <emile.generat@outlook.com>
2016-12-30 20:22:57 +0000

    Unit tests planes utils

---
+++ app/test/planes/test_planes_util.py
@@ -0,0 +1,31 @@
+import unittest
+
+from app.planes.planes_util import duration_mission, calculate_total_consumption_mission, calculate_real_autonomy_one_way
+
+
+class TestPlanesUtils(unittest.TestCase):
+    def test_duration(self):
+        distance = 10
+        speed = 6
+        dur = duration_mission(distance, speed)
+        self.assertEqual(dur, 2)
+
+    def test_consumption(self):
+        duration = 7
+        conso_per_hour = 1510
+        passengers_nb = 19
+        staff_nb = 4
+        consumption = calculate_total_consumption_mission(duration, conso_per_hour, passengers_nb, staff_nb)
+        self.assertEqual(consumption, 16579.5)
+
+    def test_real_autonomy(self):
+        kerosene_capacity = 18050
+        speed = 922
+        conso_per_hour = 1510
+        passengers_nb = 19
+        staff_nb = 4
+        real_autonomy = calculate_real_autonomy_one_way(speed, kerosene_capacity, conso_per_hour, passengers_nb, staff_nb)
+        self.assertEqual(real_autonomy, 6454)
+
+if __name__ == '__main__':
+    unittest.main()
