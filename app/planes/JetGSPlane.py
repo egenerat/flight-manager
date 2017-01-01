4661ded7b15c69413b9840deab71f98a5578604c
egenerat <emile.generat@outlook.com>
2017-01-01 20:08:25 +0000

    Create a GS plane bean and configure factory

--- app/planes/JetGSPlane.py
+++ app/planes/JetGSPlane.py
@@ -1,20 +1,20 @@
 # coding=utf-8
-from app.common.target_urls import BUY_JET_GX_URL
+from app.common.target_urls import BUY_JET_GX_URL, BUY_JET_GS_URL
 from app.planes.JetPlane import JetPlane
 
 
 class JetGXPlane(JetPlane):
 
     engines_nb = 2
-    consumption_per_hour = 2014
-    fuel_capacity = 25382
+    consumption_per_hour = 1735
+    fuel_capacity = 24000
     minimum_kerosene_before_mission = fuel_capacity
     # 8 (max hours one way) * speed * 2 (2 ways)
-    plane_range = 14480
-    price = 2650000
-    buy_url = BUY_JET_GX_URL
+    plane_range = 14464
+    price = 2800000
+    buy_url = BUY_JET_GS_URL
     plane_capacity = 19
-    speed = 905
+    speed = 904
 
     def __init__(self, **kwargs):
         super(JetGXPlane, self).__init__(**kwargs)
