3752147aee78fdca6e62c8c621714aab23fd2e53
Emile Generat <emile.generat@outlook.com>
2017-01-02 20:00:25 +0000

    configure travis to use docker

--- app/test/missions/test_missionsparsers.py
+++ app/test/missions/test_missionsparsers.py
@@ -3,7 +3,7 @@
 import requests
 
 from app.common.target_strings import TEST_MISSION_DEPARTURE_TIME
-from app.common.target_urls import YOUR_MISSIONS_URL
+from app.common.target_urls import YOUR_MISSIONS_URL, YOUR_MISSIONS_JET_URL
 from app.missions.missionparser import parse_all_missions_in_page, parse_duration_before_departure
 import unittest
 
@@ -19,7 +19,7 @@ class TestParser(unittest.TestCase):
         a = parse_duration_before_departure(TEST_MISSION_DEPARTURE_TIME)
 
     def test_jet_missions(self):
-        html_page2 = requests.get("http://localhost/AS/vos-missions-jet.html").text
+        html_page2 = requests.get(YOUR_MISSIONS_JET_URL).text
         missions_list = parse_all_missions_in_page(html_page2, '1')
         self.assertEqual(11, len(missions_list))
 
--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 cd tests_util/
 tar zvf test_pages.tar
 docker build -t fm/webserver_test_pages .
-docker run -d fm/webserver_test_pages
+docker run -d --name webserver_test_pages fm/webserver_test_pages
 export WEBSERVER_TEST_PAGES_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' webserver_test_pages)
 cd ..
+echo $WEBSERVER_TEST_PAGES_IP
