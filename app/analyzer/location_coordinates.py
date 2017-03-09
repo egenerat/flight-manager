a7339a01b2fd98a9728c9574acaf07adc81feb11
egenerat <emile.generat@outlook.com>
2017-03-09 21:40:26 +0000

    Update distance calculation

--- app/analyzer/location_coordinates.py
+++ app/analyzer/location_coordinates.py
@@ -13,9 +13,11 @@ class LocationCoordinates:
         return self.geolocator.geocode(city_name)
 
     def distance_2_points(self, city1, city2):
-        city1 = self.geolocator.geocode(city1)
+        # city1 = self.geolocator.geocode(city1)
         # print((city1.latitude, city1.longitude))
-        city2 = self.geolocator.geocode(city2)
-        if city1 and city2:
-            return int(vincenty((city1.latitude, city1.longitude), (city2.latitude, city2.longitude)).km)
+        # city2 = self.geolocator.geocode(city2)
+        # if city1 and city2:
+        lat1, long1 = city1
+        lat2, long2 = city2
+        return int(vincenty((lat1, long1), (lat2, long2)).km)
             # return int(great_circle((city1.latitude, city1.longitude), (city2.latitude, city2.longitude)).km)
