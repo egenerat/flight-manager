b8bb74050bb493df1d2deff7b4846d236b704ca3
egenerat <emile.generat@outlook.com>
2017-03-29 22:36:53 +0100

    Update package/encrypt script

--- enc_files.sh
+++ enc_files.sh
@@ -1,4 +1,5 @@
 ARCHIVE_NAME=archive.tar
-tar cvf $ARCHIVE_NAME app.yaml settings.py app/common/constants.py app/common/constants_strategy.py app/common/target_urls.py app/common/target_strings.py app/common/target_parse_strings.py app/test/parsers/resources_planes_html.py tests_util/test_pages/* app/analyzer/data/*
+tar cvf $ARCHIVE_NAME app.yaml settings.py app/common/constants.py app/common/constants_strategy.py app/common/target_urls.py app/common/target_strings.py app/common/target_parse_strings.py app/test/parsers/resources_planes_html.py tests_util/test_pages/* app/analyzer/data/**/*.{py,txt,csv}
+tar -vf $ARCHIVE_NAME --delete app/analyzer/data/input/airports.csv
 travis encrypt-file $ARCHIVE_NAME --add -f
 # git add ${ARCHIVE_NAME}.enc
