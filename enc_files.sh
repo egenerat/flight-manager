e52bd7dfb3ba0f322f16993aa74f5088261dab90
egenerat <emile.generat@outlook.com>
2017-04-03 00:38:58 +0100

    encryption new try

--- enc_files.sh
+++ enc_files.sh
@@ -1,5 +1,5 @@
 ARCHIVE_NAME=archive.tar
 tar cvf $ARCHIVE_NAME app.yaml settings.py app/common/constants.py app/common/constants_strategy.py app/common/target_urls.py app/common/target_strings.py app/common/target_parse_strings.py app/test/parsers/resources_planes_html.py tests_util/test_pages/* app/analyzer/data/**/*.{py,txt,csv}
 tar -vf $ARCHIVE_NAME --delete app/analyzer/data/input/airports.csv
-travis encrypt-file $ARCHIVE_NAME --add -f
+cat ../gpg_key |gpg --passphrase-fd 0 -c archive.tar
 # git add ${ARCHIVE_NAME}.enc
