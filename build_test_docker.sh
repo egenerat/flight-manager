38f87cae9a5b08fbb5ccfdf5572ab3fe71f38ea8
egenerat <emile.generat@outlook.com>
2017-06-03 13:45:12 +0100

    Updated local docker script Includes cleanup old container

--- build_test_docker.sh
+++ build_test_docker.sh
@@ -1,5 +1,11 @@
+old_container_id=$(docker ps -a |grep webserver_test_pages | cut -d " " -f 1)
+echo $old_container_id
+if [ -n "$old_container_id" ]; then
+	echo "cleanup all container"
+	docker rm -f $old_container_id
+fi
+
 cd tests_util/
-tar zvf test_pages.tar
 docker build -t fm/webserver_test_pages .
 docker run -d --name webserver_test_pages fm/webserver_test_pages
 export WEBSERVER_TEST_PAGES_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' webserver_test_pages)
